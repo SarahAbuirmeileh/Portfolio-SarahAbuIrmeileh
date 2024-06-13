6ae2dba353a1605b2a36e3eb6b24d86e77fca9c7
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-06-13 15:24:03 +0300

    Basic send email service

--- js/script.js
+++ js/script.js
@@ -4,3 +4,29 @@ var typed = new Typed('.typing', {
     BackSpeed: 60,
     loop: true
 })
+
+
+// (function() {
+//     emailjs.init('GWbNmnXzdkXtyMcIM'); // Replace 'YOUR_USER_ID' with your EmailJS user ID
+// })();
+
+// // Function to send email
+// function sendEmail(event) {
+//     event.preventDefault();
+    
+//     var templateParams = {
+//         name: document.getElementById('name').value,
+//         email: document.getElementById('email').value,
+//         subject: document.getElementById('subject').value,
+//         message: document.getElementById('message').value
+//     };
+    
+//     emailjs.send('service_m7y71lj', 'YOUR_TEMPLATE_ID', templateParams)
+//         .then(function(response) {
+//             console.log('SUCCESS!', response.status, response.text);
+//             alert('Email sent successfully!');
+//         }, function(error) {
+//             console.log('FAILED...', error);
+//             alert('Failed to send email.');
+//         });
+// }
