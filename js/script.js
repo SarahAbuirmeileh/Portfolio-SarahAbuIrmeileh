08dac07e9acc436b35237a542ac96bcd6d8d93bd
Sarah Abu irmeileh <211027@ppu.edu.ps>
2024-06-14 23:24:39 +0300

    Edit the portfolio and add chnge the active mode when scrolling

--- js/script.js
+++ js/script.js
@@ -14,6 +14,37 @@ navItems.forEach(item => {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    const sections = document.querySelectorAll('section');
+    const navItems = document.querySelectorAll('.nav-item');
+
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.3
+    };
+
+    const observerCallback = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                navItems.forEach(navItem => {
+                    navItem.classList.remove('active');
+                    if (navItem.getAttribute('href').substring(1) === entry.target.id) {
+                        navItem.classList.add('active');
+                    }
+                });
+            }
+        });
+    };
+
+    const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
+
+
 // (function() {
 //     emailjs.init('GWbNmnXzdkXtyMcIM'); // Replace 'YOUR_USER_ID' with your EmailJS user ID
 // })();
